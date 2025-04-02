f9adb2c2267555808717d2530a8db43887ddc65b
openmpy <150704638+openmpy@users.noreply.github.com>
2025-04-03 03:18:59 +0900

    feat: 푸터 반응형 추가 (#4)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Characters from "./pages/Characters";
 import Guild from "./pages/Guild";
@@ -9,9 +10,9 @@ import Login from "./pages/Login";
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-100">
+      <div className="min-h-screen bg-gray-100 flex flex-col">
         <Header />
-        <main className="pt-16">
+        <main className="pt-16 flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/characters" element={<Characters />} />
@@ -19,6 +20,7 @@ function App() {
             <Route path="/login" element={<Login />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   );
