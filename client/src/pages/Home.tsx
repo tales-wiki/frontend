b478d3a6b16fa49026174cfa77a535e3854d28cb
openmpy <150704638+openmpy@users.noreply.github.com>
2025-04-03 14:54:49 +0900

    feat: 인물 사전 페이지 기능 추가 (#6)
    * feat: 인물사전 뼈대 추가
    
    * style: 인물 사전 스타일 수정
    
    * refactor: 최근 편집 영역 컴포넌트 분리

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import RecentEdits from "../components/RecentEdits";
 
 const Home: React.FC = () => {
   return (
@@ -32,71 +32,7 @@ const Home: React.FC = () => {
 
         {/* 오른쪽 섹션: 최근 편집 내용 */}
         <div className="w-full lg:flex-[1]">
-          <div className="bg-white rounded-lg border border-gray-800 p-5 lg:p-6">
-            <h2 className="text-xl lg:text-xl font-semibold mb-4">최근 편집</h2>
-            <div className="space-y-4">
-              <Link to="/wiki/제목1" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목1</p>
-                  <p className="text-sm text-gray-500">방금 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목2" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목2</p>
-                  <p className="text-sm text-gray-500">5분 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목3" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목3</p>
-                  <p className="text-sm text-gray-500">15분 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목4" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목4</p>
-                  <p className="text-sm text-gray-500">30분 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목5" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목5</p>
-                  <p className="text-sm text-gray-500">1시간 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목6" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목6</p>
-                  <p className="text-sm text-gray-500">2시간 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목7" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목7</p>
-                  <p className="text-sm text-gray-500">3시간 전</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목8" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목8</p>
-                  <p className="text-sm text-gray-500">2025년 03월 13일</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목9" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목9</p>
-                  <p className="text-sm text-gray-500">2025년 03월 14일</p>
-                </div>
-              </Link>
-              <Link to="/wiki/제목10" className="block">
-                <div className="border-b pb-2">
-                  <p className="text-base font-medium">제목10</p>
-                  <p className="text-sm text-gray-500">2025년 03월 15일</p>
-                </div>
-              </Link>
-            </div>
-          </div>
+          <RecentEdits />
         </div>
       </div>
     </div>
